e50cb1dbbf6a4bf8476670e8d396616c539cce0d
Santiago Angel <santiagoangel@gmail.com>
2017-09-05 19:22:36 -0500

    Add files via upload

---
+++ apps.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+#oc login https://openshift-master.aws.sc.technology --token=XXXX
+
+
+#new project
+
+#oc new-project helloworld-msa
+
+
+#hola (JAX-RS/Wildfly Swarm) microservice
+
+git clone https://github.com/santiagoangel/hola
+cd hola/
+oc new-build --binary --name=hola -l app=hola
+mvn package; oc start-build hola --from-dir=. --follow
+oc new-app hola -l app=hola,hystrix.enabled=true
+oc expose service hola
+
+
+oc set probe dc/hola --readiness --get-url=http://:8080/api/health
+
+
+#Deploy aloha (Vert.x) microservice
+
+cd ..
+
+git clone https://github.com/redhat-helloworld-msa/aloha
+cd aloha/
+oc new-build --binary --name=aloha -l app=aloha
+mvn package; oc start-build aloha --from-dir=. --follow
+oc new-app aloha -l app=aloha,hystrix.enabled=true
+oc expose service aloha
+
+
+
+oc env dc/aloha AB_ENABLED=jolokia; oc patch dc/aloha -p '{"spec":{"template":{"spec":{"containers":[{"name":"aloha","ports":[{"containerPort": 8778,"name":"jolokia"}]}]}}}}'
+oc set probe dc/aloha --readiness --get-url=http://:8080/api/health
+
+
+#Deploy ola (Spring Boot) microservice
+
+cd ..
+
+git clone https://github.com/santiagoangel/ola
+cd ola/
+oc new-build --binary --name=ola -l app=ola
+mvn package; oc start-build ola --from-dir=. --follow
+oc new-app ola -l app=ola,hystrix.enabled=true
+oc expose service ola
+
+#Deploy bonjour (NodeJS) microservice
+
+
+cd ..
+git clone https://github.com/redhat-helloworld-msa/bonjour
+cd bonjour/
+oc new-build --binary --name=bonjour -l app=bonjour
+npm install; oc start-build bonjour --from-dir=. --follow
+oc new-app bonjour -l app=bonjour
+oc expose service bonjour
+
+oc set probe dc/bonjour --readiness --get-url=http://:8080/api/health
+
+
+oc env dc/ola AB_ENABLED=jolokia; oc patch dc/ola -p '{"spec":{"template":{"spec":{"containers":[{"name":"ola","ports":[{"containerPort": 8778,"name":"jolokia"}]}]}}}}'
+oc set probe dc/ola --readiness --get-url=http://:8080/api/health
+
+
+#Deploy api-gateway (Spring Boot)
+
+cd ..
+
+git clone https://github.com/redhat-helloworld-msa/api-gateway
+cd api-gateway/
+oc new-build --binary --name=api-gateway -l app=api-gateway
+mvn package; oc start-build api-gateway --from-dir=. --follow
+oc new-app api-gateway -l app=api-gateway,hystrix.enabled=true
+oc expose service api-gateway
+
+
+
+oc env dc/api-gateway AB_ENABLED=jolokia; oc patch dc/api-gateway -p '{"spec":{"template":{"spec":{"containers":[{"name":"api-gateway","ports":[{"containerPort": 8778,"name":"jolokia"}]}]}}}}'
+oc set probe dc/api-gateway --readiness --get-url=http://:8080/health
+
+
+#Deploy frontend (NodeJS/HTML5/JS)
+
+cd ..
+
+git clone https://github.com/redhat-helloworld-msa/frontend
+cd frontend/
+oc new-build --binary --name=frontend -l app=frontend
+npm install; oc start-build frontend --from-dir=. --follow
+oc new-app frontend -l app=frontend
+oc expose service frontend
+
+
+#Specify the OpenShift domain
+
+oc env dc/frontend OS_SUBDOMAIN=apps.aws.sc.technology
+
+
+
+oc set probe dc/frontend --readiness --get-url=http://:8080/
+
+
+#Deploy Kubeflix
+
+cd ..
+oc process -f http://central.maven.org/maven2/io/fabric8/kubeflix/packages/kubeflix/1.0.17/kubeflix-1.0.17-kubernetes.yml | oc create -f -
+oc expose service hystrix-dashboard --port=8080
+oc policy add-role-to-user admin system:serviceaccount:helloworld-msa:turbine
+
+#Enable the Hystrix Dashboard in the Frontend
+
+
+oc env dc/frontend ENABLE_HYSTRIX=true
+
+
+#Deploy Jaeger
+
+oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/0.1.2/all-in-one/jaeger-all-in-one-template.yml | oc create -f -
+oc env dc -l app JAEGER_SERVER_HOSTNAME=jaeger-all-in-one  # redeploy all services with tracing
+
+#Enable the Jaeger Dashboard in the Frontend
+
+
+oc env dc/frontend ENABLE_JAEGER=true
+
+
+#Use a SSO server to secure microservices
+
+
+oc new-project sso
+
+git clone https://github.com/redhat-helloworld-msa/sso
+cd sso/
+oc new-build --binary --name keycloak
+oc start-build keycloak --from-dir=. --follow
+oc new-app keycloak
+oc expose svc/keycloak
+
+oc set probe dc/keycloak --readiness --get-url=http://:8080/auth
+
+#Specify the OpenShift domain
+
+oc env dc/keycloak OS_SUBDOMAIN=apps.aws.sc.technology
+
+
+#Tell microservices where to find the Keycloak server
+oc project helloworld-msa
+
+# OS_SUBDOMAIN=apps.aws.sc.technology
+oc env dc KEYCLOAK_AUTH_SERVER_URL=http://keycloak-sso.apps.aws.sc.technology/auth -l app
+
+oc env dc/frontend ENABLE_SSO=true
+
+
+
---
+++ infrabuild.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+####pkgs
+
+
+sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# Install dependencies
+sudo yum -y install python-pip git python2-boto \
+python-netaddr python-httplib2 python-devel \
+gcc libffi-devel openssl-devel python2-boto3 \
+python-click python-six pyOpenSSL httpd-tools \
+java-1.8.0-openjdk-headless python-passlib PyYAML python-jinja2 python-keyczar python-paramiko sshpass
+
+# Upgrade pip
+sudo pip install --upgrade pip
+
+# Install ansible v2.2, setuptools and graffiti_monkey
+sudo pip install --upgrade setuptools graffiti_monkey
+
+# Clone repository
+sudo rm -rf /usr/share/ansible
+sudo mkdir -p /usr/share/ansible
+cd /usr/share/ansible
+sudo git clone https://github.com/openshift/openshift-ansible.git openshift-ansible
+cd openshift-ansible && sudo git checkout release-1.5
+
+
+
+#sudo rpm -Uvh https://kojipkgs.fedoraproject.org//packages/ansible/2.2.0.0/3.fc25/noarch/ansible-2.2.0.0-3.fc25.noarch.rpm
+sudo rpm -Uvh https://kojipkgs.fedoraproject.org//packages/ansible/2.2.2.0/1.fc25/noarch/ansible-2.2.2.0-1.fc25.noarch.rpm
+sudo rm -rf /usr/share/ansible-contrib
+sudo mkdir -p /usr/share/ansible-contrib
+cd /usr/share/ansible-contrib
+sudo git clone https://github.com/openshift/openshift-ansible-contrib openshift-ansible-contrib
+cd openshift-ansible-contrib/reference-architecture/aws-ansible
+
+sudo rm -f /usr/share/ansible-contrib/openshift-ansible-contrib/reference-architecture/aws-ansible/ansible.cfg
+sudo cp /home/vagrant/ocaws-design/ansible.cfg /usr/share/ansible-contrib/openshift-ansible-contrib/reference-architecture/aws-ansible/ansible.cfg
+
+sudo chown -R vagrant /usr/share/ansible*
+
+
+#run before script
+#export AWS_ACCESS_KEY_ID=XXXX
+#export AWS_SECRET_ACCESS_KEY=XXXX
+#export GITHUB_CLIENT_ID=XXXX
+#export GITHUB_CLIENT_SECRET=XXXX
+#export GITHUB_ORGANIZATION=sctechnology
+#export PUBLIC_HOSTED_ZONE=aws.sc.technology
+#export REGION=us-east-1
+
+
+./ose-on-aws.py --keypair=OSE-key --create-key=yes --key-path=/home/vagrant/.ssh/OSE-key.pub --public-hosted-zone=aws.sc.technology \
+--deployment-type=origin --ami=ami-6d1c2007 --github-client-secret=XXXX \
+--github-organization=openshift --github-organization=sctechnology --github-client-id=XXXX --deployment-type origin --stack-name santiagoinfra --containerized=false --no-confirm
+
